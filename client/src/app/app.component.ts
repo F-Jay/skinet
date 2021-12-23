9a3fec5645d4adb4f9d53f49adc7a5dbeef20ee8
F-Jay <fscholtz65@gmail.com>
2021-12-23 15:02:09 +0200

    End Of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  // Properties
   title = 'Skinet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient){ }
+
+  // Life Cycle Methods
+  ngOnInit(): void 
+  {
+    // Call API - With end point
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => 
+    {
+      this.products = response.data; // Set response from API to the Products Property to be accesible in the Html Conponent.
+    }, error =>
+    {
+      console.log(error);
+    }); 
+  }
+
+  // Life Cycle Hooks
 }
